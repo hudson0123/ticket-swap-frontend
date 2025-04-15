05763fdbf73daa31a5d9334dde8afe49da3ee99d
Hudson O'Donnell <hudsonodonnell2004@gmail.com>
2025-04-15 16:20:08 -0400

    removed old logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,7 +20,6 @@ export default function Navbar() {
         <nav class="border-gray-200 bg-gray-900 fixed w-full">
             <div class="flex flex-wrap items-center justify-between mx-auto p-4 px-10">
                 <a href={loggedIn ? "/home" : "/"} class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">UniSwap</span>
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
